65de54ca2a616cccb5dcf6dceb9c16bc87ac356b
i42vihor <i42vihor@uco.es>
2024-03-09 17:30:00 +0100

    Cambios fuchero rama master

--- include/myinclude.h
+++ include/myinclude.h
@@ -2,6 +2,8 @@
 void f(){
     char c1[100]= "Hello world";
     char c2[100]= ", I am Rosa Villarreal Hornero";
+    char c1[100]= "HOLA ";
+    char c2[100]= ", habra conflicto?";
     printf("%s\n", strcat(c1, c2));
     return 0;
 }
