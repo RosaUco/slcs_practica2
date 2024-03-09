f016d0ce9da65417e22c8d19467db1f4ed8cebf2
i42vihor <i42vihor@uco.es>
2024-03-09 16:41:55 +0100

    Now

--- include/myinclude.h
+++ include/myinclude.h
@@ -1,7 +1,7 @@
 #include <stdio.h>
 void f(){
     char c1[100]= "Hello world";
-    char c2[100]= ", I am Rosa Villarreal";
+    char c2[100]= ", I am Rosa Villarreal Hornero";
     printf("%s\n", strcat(c1, c2));
     return 0;
 }
